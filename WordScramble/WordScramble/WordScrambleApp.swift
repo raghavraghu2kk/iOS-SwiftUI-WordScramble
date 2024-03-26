6c5e710405cc4a2157bf2fef77c25ae0b02b9a15
Raghavendra Mirajkar <raghavendramirajkar@RaghavendrasAir.lan>
2024-03-26 06:23:00 +0530

    Initial project

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Raghavendra Mirajkar on 26/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
